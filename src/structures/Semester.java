f0d813249b6fda7ce6463fc10e020a769fd0d9c0
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-01-27 22:14:13 -0500

    gson Library installed.

---
+++ src/structures/Semester.java
@@ -0,0 +1,15 @@
+package structures;
+
+public class Semester {
+	
+	private int year;
+	private char session;
+	
+	public Semester(int year, char session){
+		
+		this.year = year;
+		this.session = session;
+
+	}
+	
+}
