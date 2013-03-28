6267c7fad9301c94395e836cabd82325eb7317e3
Min Rui <chenminrui@gmail.com>
2013-03-28 15:47:47 -0400

    fix imports

--- src/layout/CustomListView.java
+++ src/layout/CustomListView.java
@@ -1,14 +1,8 @@
 package layout;
 
-import java.util.List;
-
 import structures.Data;
-
-import com.example.gpaontherun.R;
-
 import android.content.Context;
 import android.graphics.Color;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
